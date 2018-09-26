fdf1ebd2d93378cb7e1a6ac5e4b0b57880d881ce
Tiago Do Prado <tprado10@icloud.com>
2018-09-26 11:01:32 -0700

    fixing error for heroku deploy

--- client/index.js
+++ client/index.js
@@ -1,8 +1,10 @@
 //client/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom'
 import Routes from './routes'
+
 
 ReactDOM.render(
   <HashRouter>
